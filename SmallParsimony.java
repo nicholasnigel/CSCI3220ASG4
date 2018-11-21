3ef77a8ff1d169ba0452e33b1367c229c59cbc0e
Nigel Nicholas <nigelnicholas@Engkans-MacBook-Air.local>
2018-11-22 02:01:17 +0800

    start coding

---
+++ SmallParsimony.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class SmallParsimony
+{
+    
+    
+    public static void main(String[] args){
+        NewickTreeReader tree_reader;
+        Scanner reader = new Scanner(System.in);   // reader object to look for inputs from console
+        String s = reader.next();
+
+        tree_reader = new NewickTreeReader(s);
+
+        
+
+        
+
+    }
+}
